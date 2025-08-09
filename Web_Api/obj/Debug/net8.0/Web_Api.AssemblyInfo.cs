906895cdbc11672822c7cb4e2a26dfd8aa836310
JonathanSise <jhonvs93@hotmail.com>
2025-08-09 01:59:24 -0500

    update 3

--- Web_Api/obj/Debug/net8.0/Web_Api.AssemblyInfo.cs
+++ Web_Api/obj/Debug/net8.0/Web_Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a17c796fc720cd54263b51513167e453f5f99ee4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece1bacb0ee1de584609d9aabcd3838cc95972b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
